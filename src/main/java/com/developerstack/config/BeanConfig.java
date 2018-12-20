8eed512760d751850b401719f50cb2e64e6d4875
ABC <example@gmail.com>
2018-12-20 14:03:28 +0530

    aaded HSQL code

--- src/main/java/com/developerstack/config/BeanConfig.java
+++ src/main/java/com/developerstack/config/BeanConfig.java
@@ -29,10 +29,15 @@ public class BeanConfig {
     @Bean
     public DataSource dataSource() {
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/test");
-        dataSource.setUsername("root");
-        dataSource.setPassword("root");
+//        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/test");
+//        dataSource.setUsername("root");
+//        dataSource.setPassword("root");
+        dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
+        dataSource.setUrl("jdbc:hsqldb:mem:test");
+        dataSource.setUsername("ha");
+      	dataSource.setPassword("");
+        
         return dataSource;
     }
 
@@ -40,7 +45,8 @@ public class BeanConfig {
         return new Properties() {
             {
                 setProperty("hibernate.hbm2ddl.auto", "update");
-                setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+               // setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+                setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
                 setProperty("hibernate.show_sql", "true");
             }
         };
